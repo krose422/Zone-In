7db2915c1aba90d0bef6c73a75c2a75367dc70e5
Kelley Rose <krose422@gmail.com>
2015-07-02 15:36:21 -0400

    Initial commit

---
+++ app/js/main.js
@@ -0,0 +1,20 @@
+;(function (){
+
+  'use strict';
+
+  angular.module('zoneinapp', ['ui.router']);
+
+    // .config(['$stateProvider', '$urlRouterProvider',
+
+    //   function ($stateProvider, $urlRouterProvider) {
+    //     $stateProvider
+    //       .state('home' {
+    //         url: '/login',
+    //         templateUrl: 'login.tpl.html',
+    //         controller: 'UserCtrl'
+    //       })
+    //   }
+
+    // ]);
+
+}());
